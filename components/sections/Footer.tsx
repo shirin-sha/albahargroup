b33d6513fc671f6550186668292518777e5e0180
shirin-sha <project.manager4565@gmail.com>
2025-12-12 18:38:44 +0400

    footer

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -12,25 +12,25 @@ const Footer = () => {
     const pathname = usePathname();
 
     let container: string = 'container-fluid';
-    let showFooterStyle1: boolean = false;
-    let showFooterStyle2: boolean = false;
-    let showFooterStyleGlobal: boolean = false;
+    // let showFooterStyle1: boolean = false;
+    // let showFooterStyle2: boolean = false;
+    // let showFooterStyleGlobal: boolean = false;
 
-    if(pathname == '/' || pathname == '/services') container = 'container';
+    // if(pathname == '/' || pathname == '/services') container = 'container';
 
-    if(pathname == '/' || pathname == '/services') {
-        showFooterStyle1 = true;
-    } else if (pathname == '/home-4') {
-        showFooterStyle2 = true;
-    } else {
-        showFooterStyleGlobal = true;
-    }
+    // if(pathname == '/' || pathname == '/services') {
+    //     showFooterStyle1 = true;
+    // } else if (pathname == '/home-4') {
+    //     showFooterStyle2 = true;
+    // } else {
+    //     showFooterStyleGlobal = true;
+    // }
 
     return (
         <footer>
-            {showFooterStyle1 && <FooterStyle1 container={container} />}
-            {showFooterStyle2 && <FooterStyle2 container={container} />}
-            {showFooterStyleGlobal && <FooterStyleGlobal container={container} />}
+           <FooterStyleGlobal container={container} />
+            {/* {showFooterStyle2 && <FooterStyle2 container={container} />}
+            {showFooterStyleGlobal && <FooterStyleGlobal container={container} />} */}
         </footer>
     )
 }
