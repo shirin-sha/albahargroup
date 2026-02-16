1b28168f65826f650b8becaf727de1731ce1adb5
shirin-sha <project.manager4565@gmail.com>
2026-02-16 14:50:48 +0400

    admin

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -4,9 +4,7 @@ const AdminDashboardPage = () => {
   return (
     <div>
       <h2 className="admin-dashboard-heading">Welcome to the Admin Dashboard</h2>
-      <p className="admin-dashboard-subtext">
-        This is your overview page. You can add stats, charts, and management tools here.
-      </p>
+     
     </div>
   );
 };
